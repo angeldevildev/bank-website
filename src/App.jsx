786ec9dfe3411164c51819beeb737fc5401b2b5d
mineshopdev <mineshop47@gmail.com>
2024-07-22 15:39:33 +0200

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
+      <Analytics />
     </div>
   )
 }
